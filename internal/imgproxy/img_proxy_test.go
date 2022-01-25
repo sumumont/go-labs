f5a675fb52c6024b8d5e18bba8daafb9eb22e249
haisen.huang <haisen.huang@apulis.com>
2022-01-25 15:13:21 +0800

    test image proxy

--- internal/imgproxy/img_proxy_test.go
+++ internal/imgproxy/img_proxy_test.go
@@ -15,7 +15,7 @@ func TestImgProxy(t *testing.T) {
 	logging.Info().Interface("config", config).Send()
 	loadconfig()
 	url := "dataset/ipc/2022012421/qijian_1643029507.jpg"
-	realUrl := Client.NewOperation().Resize(ResizeTypeFit, 1000, 100, true).AddFilePath("/" + url).GenerateURL()
+	realUrl := Client.NewOperation().Resize(ResizeTypeFit, 6000, 6000, true).AddFilePath("/" + url).GenerateURL()
 	logging.Info().Msg(realUrl)
 	fmt.Println(realUrl)
 }
