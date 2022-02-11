ea9cbde5d2cff355910a98f565f0fcdaf0f9a76e
haisen.huang <haisen.huang@apulis.com>
2022-02-11 18:55:13 +0800

    test cmd

--- cmd/go_cmd_test.go
+++ cmd/go_cmd_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"io"
-	"io/ioutil"
 	"log"
 	"os/exec"
 	"testing"
@@ -12,30 +11,31 @@ import (
 func TestA(t *testing.T) {
 	log.Println("test TestA")
 }
-func TestCmd(t *testing.T) {
-	log.Println("test cmd")
-	cmd := exec.Command("ps", "-ef", "|grep", "git")
-	log.Println(cmd.String())
-	stdout, err := cmd.StdoutPipe()
-	if err != nil { //获取输出对象，可以从该对象中读取输出结果
-		panic(err)
-	}
-	defer stdout.Close()
-	err = cmd.Run()
-	if err != nil {
-		panic(err)
-	}
-	log.Println("1")
 
-	if opBytes, err := ioutil.ReadAll(stdout); err != nil { // 读取输出结果
-		log.Println("2")
-		panic(err)
-	} else {
-		log.Println("3")
-		log.Println(string(opBytes))
-	}
-	log.Println("end")
-}
+//func TestCmd(t *testing.T) {
+//	log.Println("test cmd")
+//	cmd := exec.Command("ps", "-ef", "|grep", "git")
+//	log.Println(cmd.String())
+//	stdout, err := cmd.StdoutPipe()
+//	if err != nil { //获取输出对象，可以从该对象中读取输出结果
+//		panic(err)
+//	}
+//	defer stdout.Close()
+//	err = cmd.Run()
+//	if err != nil {
+//		panic(err)
+//	}
+//	log.Println("1")
+//
+//	if opBytes, err := ioutil.ReadAll(stdout); err != nil { // 读取输出结果
+//		log.Println("2")
+//		panic(err)
+//	} else {
+//		log.Println("3")
+//		log.Println(string(opBytes))
+//	}
+//	log.Println("end")
+//}
 func TestCmdPs(t *testing.T) {
 	ps := exec.Command("ps", "-ef")
 	grep := exec.Command("grep", "-i", "git")
