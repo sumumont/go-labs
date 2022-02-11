2f196dd2ed92b458bf857743ea3c64148358cc65
haisen.huang <haisen.huang@apulis.com>
2022-02-11 18:37:01 +0800

    test cmd

--- cmd/go_cmd_test.go
+++ cmd/go_cmd_test.go
@@ -8,7 +8,9 @@ import (
 )
 
 func TestCmd(t *testing.T) {
+	log.Println("test cmd")
 	cmd := exec.Command("ps", "-ef", "|grep git")
+	log.Println(cmd.String())
 	stdout, err := cmd.StdoutPipe()
 	if err != nil { //获取输出对象，可以从该对象中读取输出结果
 		panic(err)
