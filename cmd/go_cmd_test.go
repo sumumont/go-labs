4e3b68329f8f2b015727079b010db111ecc357d2
haisen.huang <haisen.huang@apulis.com>
2022-02-14 11:33:39 +0800

    test process

--- cmd/go_cmd_test.go
+++ cmd/go_cmd_test.go
@@ -5,10 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os/exec"
-	"strconv"
-	"syscall"
+	"strings"
 	"testing"
-	"unsafe"
 )
 
 func TestA(t *testing.T) {
@@ -41,8 +39,12 @@ func TestA(t *testing.T) {
 //}
 func TestCmdPs(t *testing.T) {
 	fmt.Println("TestCmdPs")
-	status := getProcess("git2")
-	fmt.Println(status)
+	result := getProcess("git2")
+	fmt.Println(result)
+	lines := strings.Split(result, "\n")
+	for idx, line := range lines {
+		fmt.Println("idx:", idx, " line:", line)
+	}
 }
 func getProcess(processName string) string {
 	ps := exec.Command("ps", "-ef")
@@ -60,47 +62,4 @@ func getProcess(processName string) string {
 	w.Close()
 	grep.Wait()
 	return buffer.String()
-}
-
-type ulong int32
-type ulong_ptr uintptr
-
-type PROCESSENTRY32 struct {
-	dwSize              ulong
-	cntUsage            ulong
-	th32ProcessID       ulong
-	th32DefaultHeapID   ulong_ptr
-	th32ModuleID        ulong
-	cntThreads          ulong
-	th32ParentProcessID ulong
-	pcPriClassBase      ulong
-	dwFlags             ulong
-	szExeFile           [260]byte
-}
-
-func getProcesses() {
-	kernel32 := syscall.NewLazyDLL("kernel32.dll")
-	CreateToolhelp32Snapshot := kernel32.NewProc("CreateToolhelp32Snapshot")
-	pHandle, _, _ := CreateToolhelp32Snapshot.Call(uintptr(0x2), uintptr(0x0))
-	if int(pHandle) == -1 {
-		return
-	}
-	Process32Next := kernel32.NewProc("Process32Next")
-	for {
-		var proc PROCESSENTRY32
-		proc.dwSize = ulong(unsafe.Sizeof(proc))
-		if rt, _, _ := Process32Next.Call(uintptr(pHandle), uintptr(unsafe.Pointer(&proc))); int(rt) == 1 {
-			fmt.Println("ProcessName : " + string(proc.szExeFile[0:]))
-			fmt.Println("th32ModuleID : " + strconv.Itoa(int(proc.th32ModuleID)))
-			fmt.Println("ProcessID : " + strconv.Itoa(int(proc.th32ProcessID)))
-		} else {
-			break
-		}
-	}
-	CloseHandle := kernel32.NewProc("CloseHandle")
-	_, _, _ = CloseHandle.Call(pHandle)
-}
-
-func TestProcesses(t *testing.T) {
-	getProcesses()
 }
