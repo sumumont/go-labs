9dc7c11bad0ef8ac5dbdc85dbcb728f309b2673e
haisen.huang <haisen.huang@apulis.com>
2022-02-14 11:47:26 +0800

    test process

--- cmd/go_cmd_test.go
+++ cmd/go_cmd_test.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"log"
 	"os/exec"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -69,4 +71,50 @@ func getProcess(processName string) string {
 	w.Close()
 	grep.Wait()
 	return buffer.String()
+}
+
+type Process struct {
+	pid int
+	cpu float64
+}
+
+func getPcs() {
+	cmd := exec.Command("ps", "aux")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	processes := make([]*Process, 0)
+	for {
+		line, err := out.ReadString('\n')
+		if err != nil {
+			break
+		}
+		tokens := strings.Split(line, " ")
+		ft := make([]string, 0)
+		for _, t := range tokens {
+			if t != "" && t != "\t" {
+				ft = append(ft, t)
+			}
+		}
+		log.Println(len(ft), ft)
+		pid, err := strconv.Atoi(ft[1])
+		if err != nil {
+			continue
+		}
+		cpu, err := strconv.ParseFloat(ft[2], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		processes = append(processes, &Process{pid, cpu})
+	}
+	for _, p := range processes {
+		log.Println("Process ", p.pid, " takes ", p.cpu, " % of the CPU")
+	}
+}
+
+func TestPcs(t *testing.T) {
+	getPcs()
 }
